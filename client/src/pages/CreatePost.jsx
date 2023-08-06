989c1378622a6fce8298864dad0ae2eb1e569925
jasonsonthanhngo <jasonsonthanhngo@gmail.com>
2023-08-06 00:35:26 -0700

    fix api to deploy

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,11 +17,14 @@ const CreatePost = () => {
 		if (form.prompt) {
 			try {
 				setGeneratingImg(true);
-				const response = await fetch('http://localhost:8080/api/v1/dalle', {
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify({ prompt: form.prompt }),
-				});
+				const response = await fetch(
+					'https://ai-image-generator-cqva.onrender.com/api/v1/dalle',
+					{
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({ prompt: form.prompt }),
+					}
+				);
 				const data = await response.json();
 				setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
 			} catch (error) {
@@ -38,11 +41,14 @@ const CreatePost = () => {
 		if (form.prompt && form.photo) {
 			setLoading(true);
 			try {
-				const response = await fetch('http://localhost:8080/api/v1/post', {
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify(form),
-				});
+				const response = await fetch(
+					'https://ai-image-generator-cqva.onrender.com/api/v1/post',
+					{
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify(form),
+					}
+				);
 				await response.json();
 				navigate('/');
 			} catch (error) {
